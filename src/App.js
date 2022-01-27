b9549a9e0c10083cf82a611eb52f8b1c333ae6ae
Angie <agladinop@gmail.com>
2022-01-26 22:41:22 -0500

    Fix reload page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter as Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Layout from "./components/Layout/Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import Characters from "./components/Characters/Characters.jsx";
